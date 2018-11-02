e1cf294f59108b2bfb661bdf288fae1b56209f85
kaitlyn kurfehs <Kaitlyn Kurfehs>
2018-11-02 15:17:21 -0400

    Sixth Commit

--- Assets/busmotor.cs
+++ Assets/busmotor.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Vehicles.Car;
 
-public class busmotor : MonoBehaviour {
+public class busmotor : MonoBehaviour
+{
     private float speed = 5.0f;
-   
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        
+    public GameObject Player;
 
+    void Start()
+    {
+        transform.position = (new Vector3(2.91f, 1.89f, .16f));
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.position = (new Vector3(transform.position.x, transform.position.y, Player.transform.position.z));
+    }
+    
 }
+
