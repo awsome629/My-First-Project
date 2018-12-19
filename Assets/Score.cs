144f9f5ee0349fb78207e8cc1b2a69d17bfc0ab1
kaitlyn kurfehs <Kaitlyn Kurfehs>
2018-12-19 18:54:15 -0500

    Seventh Commit

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -8,9 +8,17 @@ public class Score : MonoBehaviour {
     private int difficultyLevel = 1;
     private int maxDifficultyLevel = 10;
     private int scoreToNextLevel = 10;
+    private bool isDead = false;
     public Text scoreText;
+    public DeathMenu deathMenu;
 	// Update is called once per frame
 	void Update () {
+
+        if (isDead)
+        
+            return;
+        
+
         if (score >= scoreToNextLevel)
             LevelUp();
         score += Time.deltaTime * difficultyLevel;
@@ -24,5 +32,12 @@ public class Score : MonoBehaviour {
         difficultyLevel++;
         GetComponent<PlayerMotor>().SetSpeed(difficultyLevel);
         Debug.Log(difficultyLevel);
+    }
+    public void OnDeath()
+    {
+        isDead = true;
+        if(PlayerPrefs.GetFloat("Highscore") < score)
+            PlayerPrefs.SetFloat("Highscore", score);
+        deathMenu.ToggleEndMenu(score);
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,26 +6,54 @@ public class PlayerMotor : MonoBehaviour {
     private CharacterController controller;
     private Vector3 moveVector;
     private float speed = 5.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
     private float animationDuration = 3.0f;
-
+    private bool isDead = false;
+    private Rigidbody rb;
+    private float startTime;
 
     // Use this for initialization
     void Start () {
+        startTime = Time.time;
+        rb = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Time.time < animationDuration)
+
+        if (isDead)
+        
+            return;
+        
+        if(Time.time - startTime < animationDuration)
         {
             controller.Move(Vector3.forward * speed * Time.deltaTime);
             return;
         }
         moveVector = Vector3.zero;
 
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
         //X - left and right
         moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        if(Input.GetMouseButton(0))
+        {
+            if(Input.mousePosition.x > Screen.width / 2)
+                moveVector.x = speed;
+            else
+                moveVector.x = -speed;
+        }
         //Y - up and down
+        moveVector.y = verticalVelocity;
         //Z - forward and backward
         moveVector.z = speed;
 
@@ -35,5 +63,16 @@ public class PlayerMotor : MonoBehaviour {
     {
         speed = 5.0f + modifier;
 
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //if (rb.velocity == Vector3.zero)
+        if (hit.gameObject.tag == "Enemy")
+            Death();
+    }
+    private void Death()
+    {
+        isDead = true;
+        GetComponent<Score>().OnDeath();
     }
 }
